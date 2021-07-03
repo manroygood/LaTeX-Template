089011ccbe8d15a5ca0223c8dd759d7168335d5c
Roy Goodman <goodman@njit.edu>
2021-07-02 23:11:38 -0400

    Initial commit

---
+++ startup.m
@@ -0,0 +1,11 @@
+% MATLAB startup items
+
+% Change a bunch of settings that make figures come out closer to 
+% publication quality
+set(groot,'DefaultAxesFontSize',14)
+set(groot,'DefaultLineLineWidth',2)
+set(groot,'DefaultFunctionLineLineWidth',2)
+set(groot,'DefaultTextInterpreter','latex')
+set(groot,'DefaultAxesTickLabelInterpreter','latex')
+set(groot,'DefaultLegendInterpreter','latex');
+set(groot,'DefaultColorbarTickLabelInterpreter','latex')
