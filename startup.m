520ecfc779a810a84a1d01bb673f63b3ea3196db
Roy Goodman <31519627+manroygood@users.noreply.github.com>
2023-07-24 16:47:15 -0400

    some additional lines added to startup.m

--- startup.m
+++ startup.m
@@ -1,11 +1,31 @@
 % MATLAB startup items
 
-% Change a bunch of settings that make figures come out closer to 
-% publication quality
+% Increase the default font size in figures
 set(groot,'DefaultAxesFontSize',14)
 set(groot,'DefaultLineLineWidth',2)
 set(groot,'DefaultFunctionLineLineWidth',2)
+set(groot,'DefaultFunctionContourLineWidth',2)
+
+set(groot,'DefaultParameterizedFunctionLineLineWidth',2)
+set(groot,'DefaultContourLineWidth',1.5)
 set(groot,'DefaultTextInterpreter','latex')
 set(groot,'DefaultAxesTickLabelInterpreter','latex')
 set(groot,'DefaultLegendInterpreter','latex');
 set(groot,'DefaultColorbarTickLabelInterpreter','latex')
+set(groot,'DefaultTextFontSize',20)
+
+% Disable data tips
+if ~verLessThan('MATLAB','9.12')
+    set(groot, 'defaultAxesCreateFcn', @(ax,~) disableDefaultInteractivity(ax))
+end
+
+%set(groot,'DefaultFigureRenderer', 'painters')
+
+% Add additional directories to the path
+path1 = getenv('PATH');
+path1 = [path1 ':/usr/local/bin:/Library/TeX/texbin:/usr/X11/bin:/sw/bin:/Users/roy/bin'];
+setenv('PATH', path1);
+clear path1;
+setenv('BASH_ENV','.bash_profile');
+
+cd ~/Numerics
